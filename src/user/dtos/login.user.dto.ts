3c3f47be6c86c1f259cc162cfdf2ed847131db7d
Basil <okwutebasil01@gamil.com>
2026-01-30 12:30:47 +0100

    removed validation

--- src/user/dtos/login.user.dto.ts
+++ src/user/dtos/login.user.dto.ts
@@ -3,11 +3,9 @@ import { IsNotEmpty, IsString, Max, Min } from 'class-validator';
 export class LoginDto {
   @IsNotEmpty()
   @IsString()
-  @Max(30)
   email: string;
 
   @IsNotEmpty()
   @IsString()
-  @Max(30)
   password: string;
 }

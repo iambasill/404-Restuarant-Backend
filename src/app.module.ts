7f98e2f9a0b26b9c71ef309f60f4e8e6dc54b0a5
Basil <okwutebasil01@gamil.com>
2026-01-30 09:31:22 +0100

    modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { NotificationModule } from './notification/notification.module';
+import { OtpModule } from './otp/otp.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
+const NODE_ENV = (process.env.NODE_ENV || 'development').trim();
+console.log(`Running in ${NODE_ENV} mode`);
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: NODE_ENV === 'production' ? '.env' : `.env.${NODE_ENV}`,
+      isGlobal: true,
+    }),
+    EventEmitterModule.forRoot(),
+    DatabaseModule,
+    AuthModule,
+    UserModule,
+    NotificationModule,
+    OtpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

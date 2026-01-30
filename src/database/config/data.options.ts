fb307c4f3288e522c8d2c8cad0fb936bad71f1f9
Basil <okwutebasil01@gamil.com>
2026-01-30 10:55:55 +0100

    edited configurations

--- src/database/config/data.options.ts
+++ src/database/config/data.options.ts
@@ -3,7 +3,7 @@ import { DataSourceOptions } from 'typeorm';
 export const createDatabaseOptions = (): DataSourceOptions => ({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-
+  ssl: { rejectUnauthorized: false },
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
   synchronize: false,
--- src/database/data.source.ts
+++ src/database/data.source.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { createDatabaseOptions } from './config/data.options';
 

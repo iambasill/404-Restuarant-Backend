258e5d54a3158937af0f0dbfa32ceed0b0568057
Basil <okwutebasil01@gamil.com>
2026-01-30 10:31:04 +0100

    edited db configurations

--- src/database/config/data.options.ts
+++ src/database/config/data.options.ts
@@ -2,11 +2,7 @@ import { DataSourceOptions } from 'typeorm';
 
 export const createDatabaseOptions = (): DataSourceOptions => ({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT ?? 5432),
-  username: process.env.DB_USERNAME,
-  password: String(process.env.DB_PASSWORD), // important
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
 
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
